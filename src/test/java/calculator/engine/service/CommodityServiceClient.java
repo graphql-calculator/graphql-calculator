2c1bba4cc7d1cb95ab806bf915a28b3de0c5717a
dugenkui <dugk@foxmail.com>
2021-09-22 23:43:50 +0800

    Add nested list fetchSource unit test. (#71)

--- src/test/java/calculator/engine/service/CommodityServiceClient.java
+++ src/test/java/calculator/engine/service/CommodityServiceClient.java
@@ -17,11 +17,73 @@
 
 package calculator.engine.service;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class CommodityServiceClient {
+
+    static class SkuInfo {
+
+        private int skuId;
+
+        private int itemId;
+
+        private int sellerId;
+
+        private String color;
+
+        private int size;
+
+        public SkuInfo(int skuId, int itemId, int sellerId, String color, int size) {
+            this.skuId = skuId;
+            this.itemId = itemId;
+            this.sellerId = sellerId;
+            this.color = color;
+            this.size = size;
+        }
+
+        public int getSkuId() {
+            return skuId;
+        }
+
+        public int getItemId() {
+            return itemId;
+        }
+
+        public int getSellerId() {
+            return sellerId;
+        }
+
+        public String getColor() {
+            return color;
+        }
+
+        public int getSize() {
+            return size;
+        }
+
+        public void setSkuId(int skuId) {
+            this.skuId = skuId;
+        }
+
+        public void setItemId(int itemId) {
+            this.itemId = itemId;
+        }
+
+        public void setSellerId(int sellerId) {
+            this.sellerId = sellerId;
+        }
+
+        public void setColor(String color) {
+            this.color = color;
+        }
+
+        public void setSize(int size) {
+            this.size = size;
+        }
+    }
 
     static class ItemBaseInfo {
         private int itemId;
@@ -38,7 +100,9 @@ public class CommodityServiceClient {
 
         private int saleAmount;
 
-        public ItemBaseInfo(int itemId, int sellerId, String name, int salePrice, int stockAmount, boolean onSale, int saleAmount) {
+        private List<SkuInfo> skuList;
+
+        public ItemBaseInfo(int itemId, int sellerId, String name, int salePrice, int stockAmount, boolean onSale, int saleAmount, List<SkuInfo> skuList) {
             this.itemId = itemId;
             this.sellerId = sellerId;
             this.name = name;
@@ -46,6 +110,7 @@ public class CommodityServiceClient {
             this.stockAmount = stockAmount;
             this.onSale = onSale;
             this.saleAmount = saleAmount;
+            this.skuList = skuList;
         }
 
         public int getSaleAmount() {
@@ -114,7 +179,30 @@ public class CommodityServiceClient {
                 itemId.intValue() * 10 + 1,
                 itemId.intValue() * 5 + 1,
                 itemId.intValue() % 3 != 0,
-                itemId.intValue() * 10 + 3
+                itemId.intValue() * 10 + 3,
+                Arrays.asList(
+                        new SkuInfo(itemId.intValue(),
+                                itemId.intValue(),
+                                itemId.intValue(),
+                                "color_" + itemId,
+                                itemId.intValue()
+                        ), new SkuInfo(itemId.intValue(),
+                                itemId.intValue() * 2,
+                                itemId.intValue() * 2,
+                                "color_" + itemId,
+                                itemId.intValue() * 2
+                        ), new SkuInfo(itemId.intValue(),
+                                itemId.intValue() * 3,
+                                itemId.intValue() * 3,
+                                "color_" + itemId,
+                                itemId.intValue() * 3
+                        ), new SkuInfo(itemId.intValue(),
+                                itemId.intValue() * 4,
+                                itemId.intValue() * 4,
+                                "color_" + itemId,
+                                itemId.intValue() * 4
+                        )
+                )
         );
     }
 
